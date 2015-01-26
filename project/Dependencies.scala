55eeea662a4a28ec2735dd3bdbe714d5c6875a0b
Piotr Kozlowski <piotr.kozlowski@itcrowd.pl>
2015-01-27 00:21:15 +0100

    Dependency updates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,17 +12,17 @@ object Dependencies {
   val common = Seq(
     "com.typesafe.akka" %% "akka-actor" % Version.akka,
     "com.typesafe.akka" %% "akka-cluster" % Version.akka,
-    "com.google.guava" % "guava" % "17.0"
+    "com.google.guava" % "guava" % "18.0"
   )
 
   val webjars = Seq(
-    "org.webjars" % "requirejs" % "2.1.11-1",
-    "org.webjars" % "underscorejs" % "1.6.0-3",
-    "org.webjars" % "jquery" % "1.11.1",
-    "org.webjars" % "d3js" % "3.4.9",
-    "org.webjars" % "bootstrap" % "3.2.0" exclude ("org.webjars", "jquery"),
-    "org.webjars" % "bootswatch-yeti" % "3.2.0" exclude ("org.webjars", "jquery"),
-    "org.webjars" % "angularjs" % "1.2.16-2" exclude ("org.webjars", "jquery")
+    "org.webjars" % "requirejs" % "2.1.15",
+    "org.webjars" % "underscorejs" % "1.7.0-1",
+    "org.webjars" % "jquery" % "2.1.3",
+    "org.webjars" % "d3js" % "3.5.3",
+    "org.webjars" % "bootstrap" % "3.3.2" exclude ("org.webjars", "jquery"),
+    "org.webjars" % "bootswatch-yeti" % "3.3.2" exclude ("org.webjars", "jquery"),
+    "org.webjars" % "angularjs" % "1.3.10" exclude ("org.webjars", "jquery")
   )
 
   val metrics = Seq(
